bc2469d5fedff7eedd8aca548e627c2b7f720fab
Bimzking1 <kingofbimo@gmail.com>
2022-07-06 23:25:55 +0700

    View Product (Buyer) Done

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/info" element={<InfoProfile />} />
         <Route path="/infop" element={<InfoProduk />} />
-        <Route path="/infopb" element={<InfoProdukBuyer />} />
+        <Route path="/infopb/:id" element={<InfoProdukBuyer />} />
         <Route path="/logout" element={<Logout/>}/>
       </Routes>
     </BrowserRouter>
--- src/Components/Produk/Buyer.js
+++ src/Components/Produk/Buyer.js
@@ -1,16 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import style from './product.module.css'
 
-const Buyer = () => {
+const Buyer = (props) => {
+
   return (
     <div className={`card w-100 ${style.cardProductBuyer} mb-4`}>
-            <div className='card-body mx-2'>
-                <h5 className={`${style.namaBarang}`}>Nama Barang</h5>
-                <p className={`${style.jenisBarang}`}>Jenis Barang</p>
-                <h5 className={`${style.hargaBarang}`}>Rp Harga Barang</h5>
-                <button type="submit" class={`${style.buttonsimpan} w-100 text-white mb-3 mt-3`}>Saya tertarik dan ingin nego</button>
-            </div>
-        </div>
+      <div className='card-body mx-2'>
+        <h5 className={`${style.namaBarang}`}>{props.products[0].name}</h5>
+        <p className={`${style.jenisBarang}`}>
+          {props.products[0].category.map((productCategory) => {
+            return(
+              <div>
+                {productCategory.name}
+              </div>
+            )
+          })}
+        </p>
+        <h5 className={`${style.hargaBarang}`}>Rp. {props.products[0].price}</h5>
+        <button type="submit" class={`${style.buttonsimpan} w-100 text-white mb-3 mt-3`}>Saya tertarik dan ingin nego</button>
+      </div>
+    </div>
   )
 }
 
