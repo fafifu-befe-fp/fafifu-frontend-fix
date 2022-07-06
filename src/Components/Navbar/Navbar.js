90cdec947094d2a998da3cfe2e2416d6fca8b251
Aditya Ramday <87653433+ramd24@users.noreply.github.com>
2022-07-06 21:54:14 +0700

    navbar no responsive

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -2,37 +2,32 @@ import React from 'react'
 import style from './Navbar.module.css'
 import { Link } from 'react-router-dom'
 import {FaUser, FaBell, FaBars} from 'react-icons/fa'
+import { FiUser, FiLogIn } from 'react-icons/fi'
+import { MdNotificationsNone } from 'react-icons/md'
+import { AiOutlineUnorderedList } from 'react-icons/ai'
 
 const Navbar = () => {
   return (
-    <nav className={`navbar sticky-top d-flex justify-content-between align-items-center`}>
-      <div className='container d-flex'>
-        <div className='row d-flex justify-content-between align-items-center'>
-          <Link to="/" className='col-auto'>
-            <img src='img/logokotak.svg' alt='logoimg' className={`${style.logoKotak}`} />
-          </Link>
-          <div className='col d-flex flex-row'>
-            <form className='d-flex' role='search'>
-              <input className={`${style.searchBar} form-control`} type='search' placeholder='Cari di sini' />
-            </form>
-            <div className="col-auto d-flex mx-4">
-              <ul className="nav-menu list-inline mb-0">
-                <li className="d-flex list-inline-item">
-                  <Link to="/"  className={`${style['icons-menu']} btn d-flex align-items-center`}>
-                    <FaBars/>
-                  </Link>
-                  <Link to="/notification" type="submit" className={`${style['icons-menu']} btn d-flex align-items-center`}>
-                    <FaBell/>
-                  </Link>
-                  <Link to="/profile" type="submit" className={`${style['icons-menu']} btn d-flex align-items-center`}>
-                    <FaUser/>
-                  </Link>
-                </li>
-              </ul>                    
-            </div>
-            <Link to="/logout">
-              <button type="submit" className={`${style.buttonLogout} w-100 text-white`}>Logout</button>
-            </Link>
+    <nav className={`navbar sticky-top navbar-expand-lg`}>
+      <div className='container d-flex align-items-center justify-content-center'>
+        <Link className='navbar-brand' to='/'>
+          <img className={`${style.logoKotak}` } src='img/logokotak.svg' alt='logoSecondHand' />
+        </Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <form className="d-flex" role="search">
+            <input className={`${style.searchBar} form-control me-2`} type="search" placeholder="Cari di sini ..." aria-label="Search" />
+          </form>
+          <div className={`icon_wrapper ms-auto`}>
+            <AiOutlineUnorderedList className={`${style.aiOutline} me-3`}/>
+            <MdNotificationsNone className={`${style.mdNotif} me-3`}/>
+            <FiUser className={`${style.fiUser} me-3`}/>           
+            <button type="submit" class={`${style.buttonLogin}`}>
+              <FiLogIn className='me-1'/>
+              <Link to='/Login' className={`${style.masuk}`}>Masuk</Link>
+            </button>
           </div>
         </div>
       </div>
