f47e66b15369b0a4b953fbb2592e336e18bfb1b1
Bimzking1 <kingofbimo@gmail.com>
2022-07-19 21:59:11 +0700

    Section Terjual (Done). Todo: Form POST

--- src/Components/DaftarJual/Terjual.js
+++ src/Components/DaftarJual/Terjual.js
@@ -1,13 +1,63 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
-import Card from './DaftarJualCard';
+import Card from './TerjualCard';
+import axios from "axios";
+import style from './DaftarJual.module.css'
 
 const Terjual = () => {
-  return (
-    <>
-        <div>Terjual</div>
-    </>
-  )
+    const [productsTerjual, setProductsTerjual] = useState([])
+    const [statusCode, setStatusCode] = useState();
+        
+        useEffect(() => {
+            axios
+                .get(`https://fafifu-backend-api.herokuapp.com/v1/product/user/sold`, {
+                    headers: {
+                        Authorization: localStorage.getItem('jwtToken'),
+                    },
+                })
+                .then((res) => {
+                    setStatusCode(res.status);
+                    setProductsTerjual(res.data.data);
+                })
+                .catch((err) => {
+                    setStatusCode(err.response.status);
+                    setProductsTerjual(err.response.data.message);
+                });
+                console.log('iniprodukterjual', productsTerjual)
+    }, []);
+
+    if (statusCode === 404) {
+        return (
+          <>
+            <div className='container d-flex justify-content-center'>
+                <div className='row d-flex flex-column '>
+                    <img src='/img/diminati.png' className={`${style.wishlistImage}`}/>
+                    <div className={`${style.wishlistText} text-center`}>Barang mu terjual nih D:</div>
+                </div>
+            </div>
+
+          </>
+        );
+      }
+
+    return (
+        <>
+            <div className={`col-lg-12 py-0 d-flex flex-wrap justify-content-center justify-content-lg-between`}></div>
+                {productsTerjual.map((productTerjual) => {
+                    return(
+                        <>
+                            <div className='d-flex'>
+                                <div className={`box h-auto d-flex flex-row flex-wrap`}>
+                                    <Link to={`/infop/${productTerjual.publicId}`} className={`text-decoration-none`}>
+                                        <Card productTerjual={productTerjual}/>
+                                    </Link>
+                                </div>
+                            </div>
+                        </>
+                    )
+                })}
+        </>
+    )
 }
 
 export default Terjual
